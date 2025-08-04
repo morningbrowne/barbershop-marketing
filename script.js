bf630c1b613df5f99abf64b2468f3bd01080e640
SMEDigital <145438455+morningbrowne@users.noreply.github.com>
2025-08-04 15:00:47 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js - Barbershop Marketing
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", function () {
-    setTimeout(() => {
-      alert("Thanks for reaching out! We'll be in touch soon.");
-    }, 100);
-  });
-});
